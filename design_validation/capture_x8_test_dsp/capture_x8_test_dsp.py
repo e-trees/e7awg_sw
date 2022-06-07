9e3eb8d03132a89610288f9af09f7f724980c1ca
koike <koike@e-trees.jp>
2022-06-07 07:01:12 +0000

    add a test case to the dsp test

--- design_validation/capture_x8_test_dsp/capture_x8_test_dsp.py
+++ design_validation/capture_x8_test_dsp/capture_x8_test_dsp.py
@@ -8,7 +8,7 @@ lib_path = str(pathlib.Path(__file__).resolve().parents[2])
 sys.path.append(lib_path)
 from e7awgsw import CaptureModule, DspUnit
 
-def main(num_tests, ip_addr, capture_modules, use_labrad, server_ip_addr):
+def main(num_tests, ip_addr, capture_modules, use_labrad, server_ip_addr, only_all):
     random.seed(10)
 
     failed_tests = []
@@ -17,74 +17,81 @@ def main(num_tests, ip_addr, capture_modules, use_labrad, server_ip_addr):
         res_dir = 'result/{:03d}'.format(test_id)
         test = CaptureTestDsp(res_dir, ip_addr, capture_modules, use_labrad, server_ip_addr)
 
-        print('-- comp fir --')
-        result = test.run_test('comp_fir', DspUnit.COMPLEX_FIR)
-        if not result:
-            print('failure comp fir')
-            failed_tests.append('{} - comp_fir'.format(test_id))
+        if not only_all:
+            print('-- comp fir --')
+            result = test.run_test('comp_fir', DspUnit.COMPLEX_FIR)
+            if not result:
+                print('failure comp fir')
+                failed_tests.append('{} - comp_fir'.format(test_id))
 
-        print('\n-- decimation --')
-        result = test.run_test('decimation', DspUnit.DECIMATION)
-        if not result:
-            print('failure decimation')
-            failed_tests.append('{} - decimation'.format(test_id))
+            print('\n-- decimation --')
+            result = test.run_test('decimation', DspUnit.DECIMATION)
+            if not result:
+                print('failure decimation')
+                failed_tests.append('{} - decimation'.format(test_id))
 
-        print('\n-- real fir --')
-        result = test.run_test('real_fir', DspUnit.REAL_FIR)
-        if not result:
-            print('failure real fir')
-            failed_tests.append('{} - real_fir'.format(test_id))
+            print('\n-- real fir --')
+            result = test.run_test('real_fir', DspUnit.REAL_FIR)
+            if not result:
+                print('failure real fir')
+                failed_tests.append('{} - real_fir'.format(test_id))
 
-        print('\n-- window --')
-        result = test.run_test('window', DspUnit.COMPLEX_WINDOW)
-        if not result:
-            print('failure window')
-            failed_tests.append('{} - window'.format(test_id))
+            print('\n-- window --')
+            result = test.run_test('window', DspUnit.COMPLEX_WINDOW)
+            if not result:
+                print('failure window')
+                failed_tests.append('{} - window'.format(test_id))
 
-        print('\n-- sum --')
-        result = test.run_test('sum', DspUnit.SUM)
-        if not result:
-            print('failure sum')
-            failed_tests.append('{} - sum'.format(test_id))
+            print('\n-- sum --')
+            result = test.run_test('sum', DspUnit.SUM)
+            if not result:
+                print('failure sum')
+                failed_tests.append('{} - sum'.format(test_id))
 
-        print('\n-- integration --')
-        result = test.run_test('integ', DspUnit.INTEGRATION)
-        if not result:
-            print('failure integration')
-            failed_tests.append('{} - integ'.format(test_id))
+            print('\n-- integration --')
+            result = test.run_test('integ', DspUnit.INTEGRATION)
+            if not result:
+                print('failure integration')
+                failed_tests.append('{} - integ'.format(test_id))
 
-        print('\n-- classification --')
-        result = test.run_test('classification', DspUnit.CLASSIFICATION)
-        if not result:
-            print('failure classification')
-            failed_tests.append('{} - classification'.format(test_id))
+            print('\n-- classification --')
+            result = test.run_test('classification', DspUnit.CLASSIFICATION)
+            if not result:
+                print('failure classification')
+                failed_tests.append('{} - classification'.format(test_id))
 
-        print('\n-- all in integration path --')
-        result = test.run_test(
-            'integration_path',
-            DspUnit.COMPLEX_FIR,
-            DspUnit.DECIMATION,
-            DspUnit.REAL_FIR,
-            DspUnit.COMPLEX_WINDOW,
-            DspUnit.SUM,
-            DspUnit.INTEGRATION)
-        if not result:
-            print('failure all')
-            failed_tests.append('{} - all in integration path'.format(test_id))
+            print('\n-- all in integration path --')
+            result = test.run_test(
+                'integration_path',
+                DspUnit.COMPLEX_FIR,
+                DspUnit.DECIMATION,
+                DspUnit.REAL_FIR,
+                DspUnit.COMPLEX_WINDOW,
+                DspUnit.SUM,
+                DspUnit.INTEGRATION)
+            if not result:
+                print('failure all in integration path')
+                failed_tests.append('{} - all in integration path'.format(test_id))
 
-        print('\n-- all in classification path --')
-        result = test.run_test(
-            'classification_path',
-            DspUnit.COMPLEX_FIR,
-            DspUnit.DECIMATION,
-            DspUnit.REAL_FIR,
-            DspUnit.COMPLEX_WINDOW,
-            DspUnit.SUM,
-            DspUnit.CLASSIFICATION)
-        if not result:
-            print('failure all')
-            failed_tests.append('{} - all in classification path'.format(test_id))
+            print('\n-- all in classification path --')
+            result = test.run_test(
+                'classification_path',
+                DspUnit.COMPLEX_FIR,
+                DspUnit.DECIMATION,
+                DspUnit.REAL_FIR,
+                DspUnit.COMPLEX_WINDOW,
+                DspUnit.SUM,
+                DspUnit.CLASSIFICATION)
+            if not result:
+                print('failure all in classification path')
+                failed_tests.append('{} - all in classification path'.format(test_id))
             
+        print('\n-- all --')
+        result = test.run_test('all', *DspUnit.all())
+        if not result:
+            print('failure all')
+            failed_tests.append('{} - all'.format(test_id))
+
         print()
 
     if failed_tests:
@@ -101,6 +108,7 @@ if __name__ == "__main__":
     parser.add_argument('--capture-module')
     parser.add_argument('--server-ipaddr')
     parser.add_argument('--labrad', action='store_true')
+    parser.add_argument('--only-all', action='store_true')
     args = parser.parse_args()
 
     num_tests = 1
@@ -124,4 +132,5 @@ if __name__ == "__main__":
         ip_addr,
         capture_modules,
         args.labrad,
-        server_ip_addr)
+        server_ip_addr,
+        args.only_all)
