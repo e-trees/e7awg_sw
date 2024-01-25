7186f400feaaa24cfa910e7e43ef2d054105079f
Takefumi MIYOSHI <miyo@wasamon.net>
2024-01-26 07:51:58 +0900

    add read_versions.py
    - print versions of AWG and Capture units as human readable

---
+++ examples/misc/read_version.py
@@ -0,0 +1,20 @@
+# デバッグ用スクリプト
+import sys
+
+import argparse
+from e7awgsw import AwgCtrl, CaptureCtrl
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ipaddr')
+    args = parser.parse_args()
+    
+    if args.ipaddr is None :
+        print(f"Usage: {sys.argv[0]} --ipaddr")
+        exit(0)
+
+    addr = args.ipaddr
+
+    print("AWG Version", AwgCtrl(addr).version())
+    print("Capture Version", CaptureCtrl(addr).version())
+
